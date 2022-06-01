9777e88cf4e3a283932d5b3a4063541bf7b47fab
tanmay.bhrd <tanmay.bhrd@gmail.com>
2022-06-01 16:52:19 +0530

    few changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world! I am new to DevOps, Please give me sometime.. I will give you the good result!");
+        System.out.println("Hello world! This is a Sample Application");
 
     }
 }
