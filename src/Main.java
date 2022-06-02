58c8f9a666258710db9dcb729cf8e92738db7f93
tanmay.bhrd <tanmay.bhrd@gmail.com>
2022-06-02 08:13:42 +0530

    few changes for testing

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world! This is a Sample Application");
+        System.out.println("Just for the testing purpose");
 
     }
 }
