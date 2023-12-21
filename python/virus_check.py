b54f9e73f207067639a3f5e6b4c825615e3cf61d
iamtanmayraj <35463463+iamtanmayraj@users.noreply.github.com>
2023-12-22 01:05:19 +0530

    Update virus_check.py

--- python/virus_check.py
+++ python/virus_check.py
@@ -1,15 +1,44 @@
-import pyclamav
+import os
+import subprocess
+import tempfile
 
 def scan_file(file_path):
     try:
-        virus_found, virus_name = pyclamav.scanfile(file_path)
-        if virus_found:
-            print(f"The file '{file_path}' contains a virus: {virus_name}")
+        # Create a temporary directory to store the scanned file
+        temp_dir = tempfile.mkdtemp()
+        temp_file_path = os.path.join(temp_dir, 'uploaded_file')
+
+        # Copy the file to the temporary directory
+        os.rename(file_path, temp_file_path)
+
+        # Run the clamscan command
+        result = subprocess.run(['clamscan', temp_file_path], capture_output=True)
+
+        # Check the output for virus detection
+        output = result.stdout.decode('utf-8')
+        if "Infected files: 1" in output:
+            print(f"The file '{file_path}' contains a virus.")
         else:
             print(f"The file '{file_path}' is clean.")
     except Exception as e:
         print(f"An error occurred: {e}")
+    finally:
+        # Clean up: remove the temporary directory
+        if os.path.exists(temp_dir):
+            os.rmdir(temp_dir)
 
-# Replace 'your_file_path' with the path to the file you want to scan
-file_path = 'your_file_path'
-scan_file(file_path)
+def lambda_handler(event, context):
+    try:
+        # Assuming the file path is provided in the Lambda event
+        file_path = event.get('file_path')
+        if file_path:
+            scan_file(file_path)
+        else:
+            print("File path not provided in the event.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Example event:
+# {
+#   "file_path": "/tmp/uploaded_file.txt"
+# }
