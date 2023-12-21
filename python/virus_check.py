3f94f22dacfcde38dd9d249841e6f6f1165dc5f8
iamtanmayraj <35463463+iamtanmayraj@users.noreply.github.com>
2023-12-22 01:20:14 +0530

    Update virus_check.py

--- python/virus_check.py
+++ python/virus_check.py
@@ -1,44 +1,54 @@
 import os
 import subprocess
 import tempfile
+import boto3
 
 def scan_file(file_path):
     try:
-        # Create a temporary directory to store the scanned file
-        temp_dir = tempfile.mkdtemp()
-        temp_file_path = os.path.join(temp_dir, 'uploaded_file')
-
-        # Copy the file to the temporary directory
-        os.rename(file_path, temp_file_path)
-
         # Run the clamscan command
-        result = subprocess.run(['clamscan', temp_file_path], capture_output=True)
+        result = subprocess.run(['clamscan', file_path], capture_output=True)
 
         # Check the output for virus detection
         output = result.stdout.decode('utf-8')
         if "Infected files: 1" in output:
-            print(f"The file '{file_path}' contains a virus.")
+            return True
         else:
-            print(f"The file '{file_path}' is clean.")
+            return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
+def lambda_handler(event, context):
+    try:
+        # Specify your S3 bucket name
+        bucket_name = 'your-s3-bucket-name'
+
+        # Create an S3 client
+        s3 = boto3.client('s3')
+
+        # List objects in the specified bucket
+        objects = s3.list_objects(Bucket=bucket_name)
+
+        # Iterate through each object and scan for viruses
+        for obj in objects.get('Contents', []):
+            key = obj['Key']
+
+            # Download the file from S3 to a temporary directory
+            temp_dir = tempfile.mkdtemp()
+            temp_file_path = os.path.join(temp_dir, 'downloaded_file')
+
+            s3.download_file(bucket_name, key, temp_file_path)
+
+            # Scan the file for viruses
+            is_infected = scan_file(temp_file_path)
+
+            if is_infected:
+                print(f"The file '{key}' in the S3 bucket '{bucket_name}' contains a virus.")
+            else:
+                print(f"The file '{key}' in the S3 bucket '{bucket_name}' is clean.")
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
         # Clean up: remove the temporary directory
         if os.path.exists(temp_dir):
             os.rmdir(temp_dir)
-
-def lambda_handler(event, context):
-    try:
-        # Assuming the file path is provided in the Lambda event
-        file_path = event.get('file_path')
-        if file_path:
-            scan_file(file_path)
-        else:
-            print("File path not provided in the event.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-# Example event:
-# {
-#   "file_path": "/tmp/uploaded_file.txt"
-# }
